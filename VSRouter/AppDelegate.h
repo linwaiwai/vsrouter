97bd7a47a3e026a9300571e3c8a6af03a4ca125d
linwaiwai <linwaiwai@lazyleedeiMac.local>
2014-10-28 11:20:37 +0800

    Initial Commit

---
+++ VSRouter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VSRouter
+//
+//  Created by linwaiwai on 10/28/14.
+//  Copyright (c) 2014 Vipshop Holdings Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VSRouter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VSRouter
+//
+//  Created by linwaiwai on 10/28/14.
+//  Copyright (c) 2014 Vipshop Holdings Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
